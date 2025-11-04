90dc4a5910385b981894cd3fc723bf0293637797
Shweta Kumari <shwetakumari76543@gmail.com>
2025-11-04 21:49:36 +0530

    1st commit only health-cheack

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mera_yani_shweta_ka_app.Controllers
+{
+    [Route("/test-hl")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok(new { status = "Healthy", timestamp = DateTime.UtcNow });
+        }
+
+
+
+    }
+}
